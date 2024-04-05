476e014e11729b8a39bbc91d96ae05ab9973b7ff
klequis <klequis@gmail.com>
2024-04-05 09:18:10 -0700

    init

---
+++ README.md
@@ -0,0 +1,7 @@
+I'm thinking of a one-pager where I can input an ID and have the json response on the page
+
+- Use a signal for the ID 
+- that then triggers a refetch
+
+
+
---
+++ src/pages/users/[id].jsx
@@ -0,0 +1,34 @@
+import { A, createAsync } from "@solidjs/router";
+import { getUser } from "./[id].data";
+import { createEffect, createResource, createSignal } from "solid-js";
+
+
+export default async function Users(props) {
+  console.log('props',props)
+  const a = props.data.then((data) => console.log('data',data.result))
+  return (
+    <>
+      <h1>User</h1>
+    </>
+  );
+}
+
+// export default async function Users(props) {
+//   console.log('props',props)
+//   const [user, setUser]  = createSignal()
+//   const a = props.data.then((data) => console.log('data',data.result))
+//   console.log('a',A)
+//   const [data, { mutate, refetch }] = createResource()
+//   // console.log('data.loading',data.loading)
+//   // console.log('user',user())
+//   return (
+//     <>
+//       <h1>User</h1>
+//       <div>
+//         <p>hi</p>
+//         {/* <p>{data.loading}</p> */}
+//         <pre>{JSON.stringify(user(), null, 2)}</pre>
+//       </div>
+//     </>
+//   );
+// }
